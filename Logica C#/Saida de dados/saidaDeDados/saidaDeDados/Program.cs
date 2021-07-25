f6fa550840b465c6fd9fbbaffc77359fcd1b8695
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-07-25 11:00:50 -0300

    correção das pastas e treino saida de objetos

---
+++ Logica C#/Saida de dados/saidaDeDados/saidaDeDados/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace saidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3;
+            Console.WriteLine(a);
+
+            string s = "ABC";
+            Console.WriteLine(s);
+
+            s += "DEF";
+            Console.WriteLine(s);
+
+            int b = 10;
+            b++;
+            Console.WriteLine(b);
+
+            int c = 10;
+            c--;
+            Console.WriteLine(c);
+
+            int d = 10;
+            int e = d++;
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+        }
+    }
+}
