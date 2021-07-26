3ba7bf3cc45ab5fabe7a38d65c8a519fccf38a35
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-07-26 19:17:15 -0300

    Operadores comparativos e Operadores Logicos

---
+++ Logica C#/OperadoresLogicos/OperadoresLogicos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperadoresLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool c1 = 4 != 5; // operador "E" true
+            bool b1 = 2 > 3 && 4 != 5; // operador "E"
+            bool a1 = 2 > 3 || 4 != 5;
+            bool c2 = !(2 > 3) && 4 != 5; //true
+
+            Console.WriteLine(c1);
+            Console.WriteLine(b1);
+            Console.WriteLine(a1);
+            Console.WriteLine(c2);
+            Console.WriteLine("------------------------------");
+
+            bool c3 = 10 < 5; // false
+            bool c4 = c1 || c2 && c3; // = true
+
+            Console.WriteLine(c4);
+        }
+    }
+}
