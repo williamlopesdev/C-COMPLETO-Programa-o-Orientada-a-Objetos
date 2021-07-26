3b6820d6d5023d7dd9d834f23088c5b551914af6
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-07-26 14:00:43 -0300

    exercicio entrada de dados

---
+++ Logica C#/entrada de dados/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo:");
+            string nomeCompleto = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa:");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço de um produto:");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):"); 
+            string[] v = Console.ReadLine().Split(' ');
+
+            string ultimoNome = v[0];
+            int idade = int.Parse(v[1]);
+            double altura = double.Parse(v[2], CultureInfo.InvariantCulture);
+
+
+
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        
+        
+        }
+    }
+}
