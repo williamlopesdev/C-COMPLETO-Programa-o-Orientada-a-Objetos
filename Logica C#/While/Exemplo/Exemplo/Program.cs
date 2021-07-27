a84cc0359c487f62fd3b9a0f0137f3354dfb7555
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-07-27 19:13:17 -0300

    exemplo while

---
+++ Logica C#/While/Exemplo/Exemplo/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Globalization;
+
+namespace Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Digitar um número e mostrar sua raiz quadrada com três casas decimais,
+            //depois repetir o procedimento. Quando o usuário digitar um número
+            // negativo(podendo inclusive ser na primeira vez), mostrar uma mensagem
+            // "Número negativo" e terminar o programa
+
+
+
+            
+            Console.Write("Digite um numero: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            while (x >= 0.0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("Digite outro numero: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine("Numero negativo!");
+        }
+    }
+}
