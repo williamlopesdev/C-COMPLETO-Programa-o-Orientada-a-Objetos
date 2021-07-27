5f12754c2089fb7fbf27f283a9c1b1e3a9310f35
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-07-27 19:57:52 -0300

    Exemplo for

---
+++ Logica C#/For/Exemplo_1/Exemplo_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exemplo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Digitar um número N e depois N valores inteiros.Mostrar a soma dos N
+            //valores digitados.
+
+            Console.Write("Quantos numeros inteiros você vai digitar?");
+            int n = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += + valor;
+            }
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+}
