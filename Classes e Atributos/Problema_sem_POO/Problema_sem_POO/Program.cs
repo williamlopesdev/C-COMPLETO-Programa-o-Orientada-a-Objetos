158d50af08af23dcebf429d3a8205bbdb19f181f
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-07-27 20:59:34 -0300

    Resolvendo problema sem POO

---
+++ Classes e Atributos/Problema_sem_POO/Problema_sem_POO/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Problema_sem_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fazer um programa para ler as medidas dos lados de dois triângulos X e Y(suponha medidas
+            //válidas). Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos
+            //possui a maior área.
+            //A fórmula para calcular a área de um triângulo a partir das medidas de seus lados a, b e c é a
+            //seguinte(fórmula de Heron):
+
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("Entre com as medidas do triangulo X");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triangulo Y:");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            p = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("Area X = " + areaX.ToString("F4", CultureInfo.InvariantCulture ));
+            Console.WriteLine("Area Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture ));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior area: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior area: Y");
+            }
+
+        }
+
+    }
+}
