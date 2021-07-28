3463ee474651526b0570786b10ad2f418ff8dbaf
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-07-28 10:26:57 -0300

    Exercicios propostos Classe

---
+++ Classes e Atributos/Exercicios/Ex02/Ex02/Program.cs
@@ -0,0 +1,40 @@
+﻿
+           
+
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fazer um programa para ler os dados de um funcionário(nome,
+            //salário bruto e imposto). Em seguida, mostrar os dados do
+            //funcionário(nome e salário líquido).Em seguida, aumentar o salário
+            //do funcionário com base em uma porcentagem dada(somente o
+            //salário bruto é afetado pela porcentagem) e mostrar novamente os
+            //dados do funcionário.Use a classe projetada abaixo.
+
+            Funcionario func = new Funcionario();
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salario bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + func);
+            Console.WriteLine();
+            Console.Write("Deseja aumentar o salário em qual porcentagem? ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcent);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + func);
+        }
+    }
+}
+
+
+    
