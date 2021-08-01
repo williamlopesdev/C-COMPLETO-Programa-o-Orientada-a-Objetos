ab18ea69a826c19dd86de37b5b9c89e5020ee790
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-08-01 12:46:43 -0300

    métodos na própria classe do programa

---
+++ Classes e Atributos/Membros_staticos_1/Versao_1/Versao_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Versao_1
+{
+    class Program
+    {
+        static double Pi = 3.14;
+        static void Main(string[] args)
+        {
+            // *****Versão 1: problema resolvido com metodos na propria classe do programa
+            //Fazer um programa para ler um valor numérico qualquer, e daí mostrar
+            //quanto seria o valor de uma circunferência e do volume de uma esfera
+            //para um raio daquele valor. Informar também o valor de PI com duas
+            //casas decimais.
+
+            Console.WriteLine("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio);
+            double volume = Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        static double Circunferencia(double raio)
+        {
+            return 2.0 * Pi * raio;
+        }
+
+        static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+    }
+}
