4c847160950648b6152e321523df8c35512e8e01
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-08-07 13:39:24 -0300

    Exercicio Proposto Conversor de Moeda

---
+++ Classes e Atributos/Membros_staticos/Exercicio_Proposto/Exercicio/Exercicio_Proposto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Proposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por
+            //uma pessoa em reais. Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda
+            //que a pessoa terá que pagar 6 % de IOF sobre o valor em dólar. Criar uma classe ConversorDeMoeda
+            //para ser responsável pelos cálculos.
+
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double result = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
