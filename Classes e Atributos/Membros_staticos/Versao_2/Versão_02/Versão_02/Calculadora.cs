b30d0950b38ac99449d0f62d3eaa925069259630
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-08-07 11:13:17 -0300

    Com metodo statico

---
+++ Classes e Atributos/Membros_staticos/Versao_2/Versão_02/Versão_02/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Versão_02
+{
+    class Calculadora
+    {
+        public double Pi = 3.14;
+
+        public double Circunferencia(double raio)
+        {
+            return 2.0 * Pi * raio;
+        }
+
+        public double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+    }
+}
---
+++ Classes e Atributos/Membros_staticos/Versao_2/Versão_02/Versão_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Versão_02;
+
+namespace Versao_02
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // *****Versão 1: problema resolvido com metodos na propria classe do programa
+            //Fazer um programa para ler um valor numérico qualquer, e daí mostrar
+            //quanto seria o valor de uma circunferência e do volume de uma esfera
+            //para um raio daquele valor. Informar também o valor de PI com duas
+            //casas decimais.
+
+            Calculadora calc = new Calculadora();
+
+            Console.WriteLine("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
+
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    
+    }
+}
