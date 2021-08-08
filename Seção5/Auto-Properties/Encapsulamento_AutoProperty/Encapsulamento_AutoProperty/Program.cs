0a3f285cc9bde8654fc0f568e1f61c15dc8e5c21
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-08-08 18:58:12 -0300

    Encapsulamento: Auto-Property

---
+++ Seção5/Auto-Properties/Encapsulamento_AutoProperty/Encapsulamento_AutoProperty/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Encapsulamento_AutoProperty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
