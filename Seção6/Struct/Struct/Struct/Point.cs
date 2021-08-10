e17a6932bd4e61dffa763ffede034e0f9a1ad2d3
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-08-10 20:14:57 -0300

    tipos referencia e tipos valor

---
+++ Seção6/Struct/Struct/Struct/Point.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Struct
+{
+    struct Point
+    {
+        public double x;
+        public double y;
+
+        public override string ToString()
+        {
+            return "("+ x + ", "+ y +")";
+        }
+    }
+}
---
+++ Seção6/Struct/Struct/Struct/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p; // tipo struct não precisa instanciar;
+            p.x = 10;
+            p.y = 20;
+
+            Console.WriteLine(p);
+            p = new Point();
+            Console.WriteLine(p);
+        }
+    }
+}
