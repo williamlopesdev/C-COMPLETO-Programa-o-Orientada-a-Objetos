14fc35aae56126f8a9f5b623519733c52e858e8f
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-08-19 20:44:23 -0300

    Vetores com objetos do tipo referencia

---
+++ Seção6/Vetores/Ex02_Vetores/Ex02_Vetores/Ex02_Vetores/Produto.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Ex02_Vetores
+{
+    class Produto
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Seção6/Vetores/Ex02_Vetores/Ex02_Vetores/Ex02_Vetores/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex02_Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fazer um programa para ler um número inteiro N e os dados(nome e
+            //preço) de N Produtos.Armazene os N produtos em um vetor.Em
+            //seguida, mostrar o preço médio dos produtos.
+
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] vect = new Produto[n];
+
+            //instanciar cada um dos objetos em tipo classe
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                vect[i] = new Produto { Name = name, Price = price };
+            }
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i].Price;
+            }
+            double avg = sum / n;
+            Console.WriteLine("Preço médio = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
