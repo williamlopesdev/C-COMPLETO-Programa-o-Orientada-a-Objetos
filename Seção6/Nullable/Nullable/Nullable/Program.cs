1a0f49ed45c1eeda36040f0dc9a749cbb0a388c8
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-08-19 17:09:33 -0300

    Nullable

---
+++ Seção6/Nullable/Nullable/Nullable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System; 
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            if(x.HasValue)
+                 Console.WriteLine(x.HasValue); 
+            else
+                Console.WriteLine("X is = a null");
+
+            if(y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("Y is = null");
+
+
+            // Operador de Coalescencia nula
+
+            double? a = null;
+            double? b = 10;
+
+            double c = a ?? 5;
+            double d = b ?? 5;
+
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+
+        }
+    }
+}
