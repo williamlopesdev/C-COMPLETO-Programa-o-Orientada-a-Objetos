69c13baea40850ea4199dde5fa4455b19f2dc096
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-08-23 20:48:28 -0300

    exemplo ref e out, code smells

---
+++ Seção6/Ref_e_Out/Ref_e_Out/Ref_e_Out/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ref_e_Out
+{
+    class Calculator
+    {
+        public static void Triple(ref int x)
+        {
+            x = x * 3;
+        }
+
+        public static void Triple2(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ Seção6/Ref_e_Out/Ref_e_Out/Ref_e_Out/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ref_e_Out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+
+
+            int b = 10;
+            int triple2;
+            Calculator.Triple2(b, out triple2);
+            Console.WriteLine(triple2);
+
+        }
+
+    }
+}
