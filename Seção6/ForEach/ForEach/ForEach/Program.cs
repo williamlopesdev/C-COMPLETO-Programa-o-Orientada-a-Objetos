190a2cbb5ac43311ab8c37cbd37cc88ff1490a75
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-08-23 21:11:47 -0300

    foreach exemplo

---
+++ Seção6/ForEach/ForEach/ForEach/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "Alex", "Bob" };
+
+            for (int i = 0; i < vect.Length; i++)
+            {
+                Console.WriteLine(vect[i]);
+            }
+            Console.WriteLine("__________________________");
+
+            foreach(string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
