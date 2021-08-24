90d68c4d68dbe39fa10e1633ce000dbeb256dafa
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-08-24 20:02:49 -0300

    exemplos matriz

--- Seção6/Listas/Exercicios_Propostos/Exercicios_Propostos/Empregados.cs
+++ Seção6/Listas/Exercicios_Propostos/Exercicios_Propostos/Empregados.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Exercicios_Propostos
 {
     class Empregados
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Empregados(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            Salary += Salary * percentage / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
     }
 }
---
+++ Seção6/Matriz/Matriz/Matriz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2, 3];
+
+            Console.WriteLine(mat.Length); //imprime quantas posições a matriz tem
+
+            Console.WriteLine(mat.Rank); //mostra a quantidade de linhas
+
+            Console.WriteLine(mat.GetLength(0)); // quantidde de linhas da matriz
+
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
