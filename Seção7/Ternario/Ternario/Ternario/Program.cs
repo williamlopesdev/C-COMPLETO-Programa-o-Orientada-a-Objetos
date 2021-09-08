06450e357fa19088958bd8212b90e09771443a89
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-09-08 20:03:47 -0300

    ternario

---
+++ Seção7/Ternario/Ternario/Ternario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ternario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05; //condição ternaria
+
+
+            //if (preco < 20.0)
+            //{
+            //    desconto = preco * 0.1;
+            //}
+            //else
+            //{
+            //    desconto = preco * 0.05;
+            //}
+        }
+    }
+}
