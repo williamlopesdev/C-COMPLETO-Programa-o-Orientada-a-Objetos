05ccb7df158679fc8fae926b4656d3cbb2cc6325
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-09-12 09:50:19 -0300

    Programa que lê o valor do contrato de um trabalhador, utilizando enumeração e entidades

---
+++ Secao9/Exercicios/Course/Course/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Course.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Secao9/Exercicios/Course/Course/Entities/Enums/workerLevel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Course.Entities.Enums
+{
+    enum workerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
