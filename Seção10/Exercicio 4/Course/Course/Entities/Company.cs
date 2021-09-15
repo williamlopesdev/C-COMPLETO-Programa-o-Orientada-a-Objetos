0f142d90a239084084ff3d561a20837f12dca33b
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-09-15 09:30:37 -0300

    Try Catch

--- Seção10/Exercicio 4/Course/Course/Entities/Company.cs
+++ Seção10/Exercicio 4/Course/Course/Entities/Company.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace Course.Entities
 {
-    class Company
+    class Company : TaxPayer
     {
+        public int NumberOfEmployees { get; set; }
+
+        public Company()
+        {
+        }
+
+        public Company(string name, double anualIncome, int numberOfEmployees)
+            : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            if (NumberOfEmployees > 10)
+            {
+                return AnualIncome * 0.14;
+            }
+            else
+            {
+                return AnualIncome * 0.16;
+            }
+        }
     }
 }
--- Seção10/Exercicio 4/Course/Course/Entities/TaxPayer.cs
+++ Seção10/Exercicio 4/Course/Course/Entities/TaxPayer.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace Course.Entities
 {
-    class TaxPayer
+    abstract class TaxPayer
     {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public TaxPayer()
+        {
+        }
+
+        protected TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
     }
 }
