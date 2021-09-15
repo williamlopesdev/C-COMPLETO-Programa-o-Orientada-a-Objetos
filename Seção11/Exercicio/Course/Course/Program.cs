f41ade018c92b54f13ba20b8742beb65d70e575f
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-09-15 20:10:28 -0300

    exercicio proposto

---
+++ Seção11/Exercicio/Course/Course/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Course.Entities;
+using Course.Entities.Exceptions;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fazer um programa para ler os dados de uma conta bancária e depois realizar um
+            //saque nesta conta bancária, mostrando o novo saldo. Um saque não pode ocorrer
+            //ou se não houver saldo na conta, ou se o valor do saque for superior ao limite de
+            //saque da conta.Implemente a conta bancária conforme projeto abaixo        }
+
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            String holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account acc = new Account(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                acc.Withdraw(amount);
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
