6cd62e8b6e4e0a2f6b80bca68d42ca645a1c3dd7
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-09-15 10:27:55 -0300

    criado sem tratamento de exceções e com logica sem delegação

---
+++ Seção11/our exception/Course/Course/Entities/Reservation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", Check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", Check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nigths: ";
+        }
+    }
+}
---
+++ Seção11/our exception/Course/Course/Program.cs
@@ -0,0 +1,62 @@
+﻿using Course.Entities;
+using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fazer um programa para ler os dados de uma reserva de hotel(número do quarto, data
+            //de entrada e data de saída) e mostrar os dados da reserva, inclusive sua duração em
+            //dias.Em seguida, ler novas datas de entrada e saída, atualizar a reserva, e mostrar
+            //novamente a reserva com os dados atualizados. O programa não deve aceitar dados
+            //inválidos para a reserva, conforme as seguintes regras:
+            //-Alterações de reserva só podem ocorrer para datas futuras
+
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("check in date (aa/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("check out date(aa / MM / yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if(checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("check in date (aa/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("check out date(aa / MM / yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+
+                if (checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("Errror in reservation: Reservation dates for update must be future dates");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Errror in reservation: Check-out must be after check-in date");
+                }
+                else
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+
+            }
+        }
+    }
+}
