15faaeb041b2b85b6a80e6e8c553d39ab4c894cd
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-09-20 20:02:33 -0300

    streamWriter

--- Secao13/Bloco/Course/Course/Program.cs
+++ Secao13/Bloco/Course/Course/Program.cs
@@ -10,17 +10,21 @@ namespace Course
             string path = @"c:\temp\file1.txt";
             try
             {
-                using (FileStream fs = new FileStream(path, FileMode.Open))
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    using (StreamReader sr = new StreamReader(fs))
-                    {
-                        while (!sr.EndOfStream)
-                        {
-                            string line = sr.ReadLine();
-                            Console.WriteLine(line);
-                        }
-                    }
+
                 }
+                //using (FileStream fs = new FileStream(path, FileMode.Open))
+                //{
+                //    using (StreamReader sr = new StreamReader(fs))
+                //    {
+                //        while (!sr.EndOfStream)
+                //        {
+                //            string line = sr.ReadLine();
+                //            Console.WriteLine(line);
+                //        }
+                //    }
+                //}
             }
             catch(IOException e)
             {
